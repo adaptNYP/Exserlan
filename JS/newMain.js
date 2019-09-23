a4e91521b1d3ab23de4e07b5937a1d261dbd1a66
Benny <bennyrobot1143@gmail.com>
2019-09-23 22:06:41 +0800

    increase bypass just incase

--- JS/newMain.js
+++ JS/newMain.js
@@ -320,7 +320,7 @@ const data = new (class {
   }
 
   dataNewTime(date) {
-    date = new Date(date.getTime()+500) //Hacks
+    date = new Date(date.getTime() + 1000); //Hacks
     this.nameArray = this.arrayByNames(this.dayData, date);
     jasonView(this.nameArray);
     this.qnLabelArray = this.arrayByQnLabel(this.dayData, date);
